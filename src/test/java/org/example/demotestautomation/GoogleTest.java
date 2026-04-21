2495b8488c9b0b76b3fd5c3a37296d4bd9431c5f
Carlos Jaid Nieves <cjaid@Jaid-MacBook-Pro.local>
2026-04-20 21:20:59 -0600

    new version for GoogleTest.java

--- src/test/java/org/example/demotestautomation/GoogleTest.java
+++ src/test/java/org/example/demotestautomation/GoogleTest.java
@@ -4,8 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.ui.WebDriverWait;
+//import org.openqa.selenium.ui.ExpectedConditions;
 
 public class GoogleTest {
+    // version 1
+    /*
     public static void main(String[] args) {
 
         // 1. Open browser
@@ -35,5 +43,35 @@ public class GoogleTest {
         
         // 8. Quit the browser
         gDriver.quit();
+    }
+    */
+    // version 2
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.mx");
+
+        // Wait setup
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Find search box
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+
+        searchBox.sendKeys("Amazon SDET jobs");
+        searchBox.submit();
+
+        // Wait for results page title to contain keyword
+        wait.until(ExpectedConditions.titleContains("SDET"));
+
+        // Read data: first assertion mindset
+        if(driver.getTitle().contains("Amazon")){
+            System.out.println("Test passed");
+        }{
+            System.out.println("Test failed");
+        }
+
+
+
+        driver.quit();
     }
 }
